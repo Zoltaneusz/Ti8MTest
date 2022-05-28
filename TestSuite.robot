6c285d5a2cb1724f8207df3a25ffcb9888747821
Zoltán Fuszenecker <fzoltan88@gmail.com>
2022-05-28 11:46:59 +0200

    Use Case Stellenabo + Performance Test
    Library Functions:
    start_timer
    stop_timer
    get_timer
    click_button
    fill_jobabo_form_page_1
    fill_jobabo_form_page_2
    
    Robot Script now tests performance

--- TestSuite.robot
+++ TestSuite.robot
@@ -4,6 +4,8 @@ Documentation     A test suite for valid login.
 ...               Keywords are imported from the resource file
 Resource          keywords.resource
 Default Tags      positive
+Test Teardown     Disconnect Webdriver
+
 
 *** Variables ***
 @{EXPECTED_JOBS}    Software Engineer mit Flair für Machine Learning    Data Architect mit einem Flair für AI    Cloud Data Architect mit Flair für AI
@@ -12,29 +14,33 @@ Default Tags      positive
 TC1 Behaviour Driven Scenario
 	Given Connected To Website
 	When The User Scrolls To Search
-	And Inputs The Word Machine
+	And Inputs The Word(s) Machine
 	And Selects Seniority Senior
 	Then The Job Result Should Be Cloud Data Architect mit Flair für AI
+	
 
 TC2 Behaviour Driven Performance Test
 	Given Connected To Website
 	When The User Scrolls To Search
-	And Inputs The Word Machine
-	And Selects Seniority Senior
-	Then The Job Result Should Be Cloud Data Architect mit Flair für AI
+	And Inputs The Word(s) Machine Test Engineering Python
+	Then The Expected Job Number Is 100
+	Then The Time Should Take Maximum 20
+	
+	
 
 TC3 Verify Location Consistency
     Connect to Website
 	Search Keyword in Joblist    Düsseldorf
 	Verify Result Location    Düsseldorf
-	Disconnect
+
+	
 
 TC4 Search in Field and Click Link
     Connect to Website
 	Search Keyword in Joblist    Machine
 	Verify Result    @{EXPECTED_JOBS}
 	Click Links
-	Disconnect
+	
 
 
 	
--- Ti8MLibrary.py
+++ Ti8MLibrary.py
@@ -14,6 +14,7 @@ class Ti8MLibrary:
         self.job_list = []
         self.first_tab = ''
         self.search_iframe = None
+        self.elapsed_time = 0
         self.seniority_array = [["Junior", "Professional", "Senior"],
                                 ["1301737", "1301738", "1301739"]]
         
@@ -125,15 +126,68 @@ class Ti8MLibrary:
     def disconnect_webdriver(self):
         self.driver.quit()
 
-# Ti8m=Ti8MLibrary()
-# Ti8m.connect('C:\Program Files (x86)\geckodriver.exe', 'https://www.ti8m.com/de/career')
-# Ti8m.load_and_switch_to_iframe()
+    def start_timer(self):
+        self.elapsed_time = time.perf_counter()
+        
+    def stop_timer(self):
+        self.elapsed_time = time.perf_counter()-self.elapsed_time
+        
+    def get_timer(self):
+        return self.elapsed_time
+    
+    
+    def click_button(self,search_param, btn):
+        if search_param == "ID":
+            by = By.ID
+        elif search_param == "Class":
+            by = By.CLASS_NAME
+        button = self.job_list = WebDriverWait(self.driver, 5).until(
+            EC.presence_of_element_located((by, btn)
+            ))
+        button.send_keys(Keys.RETURN)
+        time.sleep(3)
+    
+    def fill_jobabo_form_page_1(self, stichwort, standort, bereich, seniorität):
+        input_stichwortsuche = WebDriverWait(self.driver, 5).until(
+            EC.presence_of_element_located((By.NAME, "query"))
+            )
+        input_stichwortsuche.send_keys(stichwort)
+        
+        #TODO...fill other checkboxes
+        self.click_button("ID", "single-opt-in")
+
+    def fill_jobabo_form_page_2(self, bezeichnung, email):
+        input_bezeichnung = WebDriverWait(self.driver, 5).until(
+            EC.presence_of_element_located((By.NAME, "jobabo_bezeichnung"))
+            )
+        input_bezeichnung.send_keys(bezeichnung)
+        
+        input_email = WebDriverWait(self.driver, 5).until(
+            EC.presence_of_element_located((By.NAME, "jobabo_email"))
+            )
+        input_email.send_keys(email)
+        self.click_button("Class", "jobabo-subscribe-button")
+    
+      
+    
+
+Ti8m=Ti8MLibrary()
+Ti8m.connect('C:\Program Files (x86)\geckodriver.exe', 'https://www.ti8m.com/de/career')
+Ti8m.load_and_switch_to_iframe()
+# Ti8m.start_timer()
 # Ti8m.search_in_field('Machine')
 # Ti8m.search_in_seniority("Senior")
-# time.sleep(5)
-# # Ti8m.list_job_results()
-# # print(Ti8m.get_list_of_job_results())
-# # print(Ti8m.get_size_of_job_results())
-# # Ti8m.click_link_of_job_result("Professional Python Engineer")
-# # print(Ti8m.get_job_result_text(0))
-#
+#time.sleep(5)
+# Ti8m.list_job_results()
+# Ti8m.list_job_results_with_timeout(0.5)
+# print(Ti8m.get_list_of_job_results())
+# Ti8m.stop_timer()
+# print(Ti8m.get_size_of_job_results() == 1)
+# print(Ti8m.get_timer())
+# Ti8m.click_link_of_job_result("Professional Python Engineer")
+# print(Ti8m.get_job_result_text(0))
+Ti8m.click_button("ID", "jobabo-subscribe-button")
+Ti8m.fill_jobabo_form_page_1("Python", "Zürich", "Engineering", "Senior")
+Ti8m.fill_jobabo_form_page_2("Header", "fzoltan88@gmail.com")
+
+Ti8m.disconnect_webdriver()
