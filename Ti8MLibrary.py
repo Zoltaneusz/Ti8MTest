c793fb4b435b9fff084a6236b66069d0471b320b
Zoltán Fuszenecker <fzoltan88@gmail.com>
2022-05-29 11:54:07 +0200

    Jobabo network traffic interception started

--- Ti8MLibrary.py
+++ Ti8MLibrary.py
@@ -1,9 +1,10 @@
-from selenium import webdriver
+#from selenium import webdriver
 import time
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from seleniumwire import webdriver
 
 class Ti8MLibrary:
 
